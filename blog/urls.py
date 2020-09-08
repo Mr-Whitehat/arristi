cab7764d1a586da00dc84f5b4d2b773b95010402
Mr-Whitehat <mr.whitehatmr.whitehat@gmail.com>
2020-09-08 13:19:38 +0530

    13:20 08September2020 COMMIT

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='bloghome'),
-    path("blogpost/", views.blogpost, name="blogpost") #<int:id>
+    path("blogpost/<int:id>", views.blogpost, name="blogpost") #<int:id>
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,10 @@ from .models import Blogpost
 # Create your views here.
 def index(request):
     post = Blogpost.objects.all()
-    # print(post)
-    return render(request, 'blog/index.html', {'post': post})
+    print(post)
+    return render(request, 'blog/index.html',{'post': post})
 
-def blogpost(request):
-    # post = Blogpost.objects.filter(post_id=id)
-    return render(request, 'blog/blogpost.html')
+def blogpost(request,id):
+    post = Blogpost.objects.filter(post_id=id)[0]
+    print(post)
+    return render(request, 'blog/blogpost.html',{'post': post})
